a2b02ef83869644c8d4a2ceeb2013d664d835699
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-02-21 17:21:54 +0700

    ADD: Added routes and pages to resources

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+
+Route::get('/karyawan', function () {
+    return view('karyawan');
+});
+
+Route::get('/keuangan', function () {
+    return view('keuangan');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/transaksi', function () {
+    return view('transaksi');
+});
+
+Route::get('/masuk', function () {
+    return view('sign-in');
+});
+
+Route::get('/daftar', function () {
+    return view('sign-up');
 });
